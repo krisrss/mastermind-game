16367e1b451310dfe224df3ddeb97cbe430f83ef
Kris <kristapsrss@gmail.com>
2018-01-21 18:31:35 +0200

    Add choice validation and display results

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,6 +14,46 @@
         return creatElement;
     };
 
+    const GameBoard = {
+        currentRow: null,
+        pebbleNr: 4,
+        gameColors: ["red", "green", "blue", "yellow", "brown", "orange", "black", "white"],
+        codeToGuess: ["red", "green", "black", "white"],
+        clickedColors: [],
+        validationResult: [],
+
+        //Apply correct pebbles, to correct pebble list
+        applyCorrectPebbles: function () {
+            for (let i = 0; i < this.pebbleNr; i++) {
+                const correctList = createDivEl("correct-pebble");
+                correctList.textContent = "?";
+                correctPebbleList.appendChild(correctList);
+            };
+        },
+
+        //Apply attempt rows to the game board
+        applyGameRows: function () {
+            for (let i = 0; i < 10; i++) {
+                attemptContent.appendChild(wrapRowElements());
+            };
+        }
+    };
+
+
+    //Initialize the game board
+    applyColors(optionPebbles, GameBoard.gameColors);
+    GameBoard.applyCorrectPebbles();
+    GameBoard.applyGameRows();
+    GameBoard.currentRow = document.querySelectorAll(".attempt-el-wrapper")[9];
+    selectCheckButton(GameBoard.currentRow).style.visibility = "visible";
+
+    //Apply an array of colors, to a list of game elements
+    function applyColors(elements, color) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor = color[i];
+        }
+    };
+
 
     //Create div element with a class name
     function createDivEl(className) {
@@ -29,6 +69,15 @@
         return pebbleList;
     };
 
+    //Create a submit button
+    function confirmButton() {
+        const button = newElement("button", "className", "submitButton");
+        button.textContent = "Check";
+        button.style.visibility = "hidden";
+        return button;
+    };
+
+
     //Create a list that wraps all row elements together
     function wrapRowElements() {
         const wrapper = createDivEl("attempt-el-wrapper");
@@ -38,57 +87,100 @@
         wrapper.appendChild(
             createPebbleList("result-list", "result-pebble", GameBoard.pebbleNr)
         );
+        wrapper.appendChild(
+            confirmButton()
+        );
         return wrapper;
     };
 
-    //Apply an array of colors, to a list of game elements
-    function applyColors(elements, color) {
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].style.backgroundColor = color[i];
-        }
+    //-----Code above creates the boards-----//
+
+
+
+
+
+    //----Code below creates functionality-----//
+
+
+
+    //Select last element of parent div
+    function selectCheckButton(parentElement) {
+        return parentElement.lastElementChild;
     };
 
-
     //Select all attempt-pebbles, from their parent div
-    function selectAttempPebbles(parentElement) {
+    function selectAttemptPebbles(parentElement) {
         const selectList = parentElement.firstElementChild;
         return selectList.querySelectorAll(".attempt-pebble");
     };
 
+    //Select result pebbles from current row
+    function selectResultPebbles(currentRow) {
+        const selectList = selectCheckButton(currentRow).previousElementSibling;
+        return selectList.querySelectorAll(".result-pebble");
+    }
 
-    const GameBoard = {
-        pebbleNr: 4,
-        currentRow: null,
-        clickedColors : [],
-        gameColors: ["red", "green", "blue", "yellow", "brown", "orange", "black", "white"],
+    //Check if arrays are identical
+    function arraysEqual(clickedCol, generatedCol) {
+        for (let i = clickedCol.length; i--;) {
+            if (clickedCol[i] !== generatedCol[i])
+                return false;
+        }
+        return true;
+    }
 
-        //Apply correct pebbles, to correct pebble list
-        applyCorrectPebbles: function () {
-            for (let i = 0; i < this.pebbleNr; i++) {
-                const correctList = createDivEl("correct-pebble");
-                correctList.textContent = "?";
-                correctPebbleList.appendChild(correctList);
+    //Validate the input that is provided by user
+    function validateUserGuess(clickedColors, generatedColors) {
+        if (arraysEqual(clickedColors, generatedColors) === false) {
+            for (let i = 0; i < clickedColors.length; i++) {
+                produceValidationResult(clickedColors, generatedColors, i);
             };
-        },
-        //Apply attempt rows to the game board
-        applyGameRows: function () {
-            for (let i = 0; i < 10; i++) {
-                attemptContent.appendChild(wrapRowElements());
+            fillWithBlanks(clickedColors);
+        }
+        else {
+            for (let i = 0; i < clickedColors.length; i++) {
+                GameBoard.validationResult[i] = "red";
             };
         }
     };
 
-    //Initialize the game board
-    applyColors(optionPebbles, GameBoard.gameColors);
-    GameBoard.applyCorrectPebbles();
-    GameBoard.applyGameRows();
-    GameBoard.currentRow = document.querySelectorAll(".attempt-el-wrapper")[9];
+    //Produce validation result list, displaying which colors were guessed
+    function produceValidationResult(clickedCol, generatedCol, index) {
+        if (clickedCol.indexOf(generatedCol[index]) > -1 && generatedCol[index] != clickedCol[index]) {
+            GameBoard.validationResult.push("grey");
+        }
+        else if (generatedCol[index] === clickedCol[index]) {
+            GameBoard.validationResult.unshift("red");
+        }
+
+    }
+
+    //Fill the remaing user guess with blanks, if user didn't guess anything
+    function fillWithBlanks(clickedCol) {
+        for (let x = 0; x < clickedCol.length; x++) {
+            if (GameBoard.validationResult[x] == null) {
+                GameBoard.validationResult.push("white");
+            }
+        }
+    }
+
+
+
+    //Validate user choice by clicking select button
+    attemptContent.addEventListener("click", function (el) {
+        const clickedEl = el.target;
+        if (clickedEl.className === "submitButton") {
+            validateUserGuess(GameBoard.clickedColors, GameBoard.codeToGuess);
+            applyColors(selectResultPebbles(GameBoard.currentRow), GameBoard.validationResult);
+        }
+    });
+
 
 
     //Apply "select", click event to all option pebbles
     optionList.addEventListener("click", function (el) {
         const selectedEl = el.target;
-        const selectPebbles = selectAttempPebbles(GameBoard.currentRow);
+        const selectPebbles = selectAttemptPebbles(GameBoard.currentRow);
 
         if (selectedEl.className === "option-pebble" && GameBoard.clickedColors.length < GameBoard.pebbleNr) {
             GameBoard.clickedColors.push(selectedEl.style.backgroundColor);
